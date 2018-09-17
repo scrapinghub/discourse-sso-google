5e0709c39cf204c546b6ef29daa6407ef0430bd0
Daniel Graña <dangra@gmail.com>
2018-09-17 18:22:27 -0300

    read settings from env

--- sso/settings.py
+++ sso/settings.py
@@ -1,14 +1,23 @@
 import os
 
-# Secret is for demo purpose and same published at 
+DISCOURSE_SECRET = os.getenv(
+    'DISCOURSE_SECRET',
+# Secret is for demo purpose and same published at
 # https://meta.discourse.org/t/official-single-sign-on-for-discourse-sso/13045
-DISCOURSE_SECRET = os.getenv('DISCOURSE_SECRET', 'd836444a9e4084d5b224a60c208dce14')
-DISCOURSE_SSO_LOGIN = 'http://my-discourse-site/session/sso_login'
+    'd836444a9e4084d5b224a60c208dce14'
+)
+DISCOURSE_SSO_LOGIN = os.getenv(
+    'DISCOURSE_SSO_LOGIN',
+    'http://my-discourse-site/session/sso_login'
+)
 
 GOOGLE_AUTH_URL = 'https://accounts.google.com/o/oauth2/v2/auth'
-GOOGLE_CLIENT_ID = 'Get it from Google API console'
-GOOGLE_SECRET = 'Get it from Google API console'
-GOOGLE_REDIRECT_URI = 'http://localhost:5000/discourse/sso/google-oauth2/callback'
+GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
+GOOGLE_SECRET = os.getenv('GOOGLE_SECRET')
+GOOGLE_REDIRECT_URI = os.getenv(
+    'GOOGLE_REDIRECT_URI',
+    'http://localhost:5000/discourse/sso/google-oauth2/callback'
+)
 
 try:
     from local_settings import *
